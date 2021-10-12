51e77e7c8ef5ddf7068a164e294301ac952c4b14
Ajfriel86 <angus.friel.1986@gmail.com>
2021-10-12 08:39:24 +0000

    added comments to run.py

--- run.py
+++ run.py
@@ -1,7 +1,18 @@
 from random import randint
 
+"""
+For the players and computers board I created
+an empty array to hold the siymbols which dictated
+what is happening on the board
+"""
+
 comp_board = []
 player_board = []
+
+"""
+The following function is triggered at the start of the game
+It prints out the rules to show players how the games works
+"""
 
 
 def game_begin():
@@ -19,13 +30,28 @@ def game_begin():
 
 game_begin()
 
+"""
+The following for loop creates an arrary of 8 rows
+It then populates this array with ~ which represents water
+"""
+
 for x in range(8):
     comp_board.append(["~"] * 8)
+
+"""
+The following fuction removes the [] from the for loop array above
+"""
 
 
 def comp_board_app(comp_board):
     for grid in comp_board:
         print(" ".join(grid))
+
+
+"""
+The following two fuctions pick a random loction within the (0,7) range
+for both rows and columns
+"""
 
 
 def rand_row(comp_board):
@@ -36,19 +62,25 @@ def rand_col(comp_board):
     return randint(0, len(comp_board[0]) - 1)
 
 
-rand_col(comp_board)
-rand_row(comp_board)
+"""
+The varibales below are the varibles used to store the computers ship
+"""
 
 comp_row = rand_row(comp_board)
 comp_col = rand_col(comp_board)
 
 
 """
-Comments
+The following for loop creates the players board
+the same way the computers board was created
 """
 
 for y in range(8):
     player_board.append(["~"] * 8)
+
+"""
+This function removes the [] from the players board
+"""
 
 
 def player_board_app(player_board):
@@ -57,38 +89,66 @@ def player_board_app(player_board):
 
 
 """
-Comments
+The following function 'game' is the main controls for this game
+all game tasks are carried out in this funtion
 """
 
 
 def game(comp_board, comp_col, comp_row, comp_board_app, player_board):
 
+    """
+    These varibles stores the number of ships
+    on the players and computerts board
+    """
     player_destroyed = 0
     comp_destroyed = 0
 
+    """
+    This varible asks for the users name, stores it
+    and then reads it back to them with a welcome mnessagw
+    """
     players_name = input("Please enter your name: \n")
     print(f"Welcome to War {players_name}.")
-
+    """
+    Varibles that store the players ship row and coloum
+    """
     player_row = int(input("Choose your ships row: \n"))
     player_col = int(input("Choose your ships column: \n"))
-    if (player_row < int(0) or player_row > int(7)) \
-            or (player_col < int(0) or player_col > int(7)):
+    """
+    If statement to see that the players selects the correct
+    coordinates within the board to place their ship
+    """
+    if (player_row < 0 or player_row > 7) \
+            or (player_col < 0 or player_col > 7):
         print("You\'re way off course solider!!!")
         print("Choose between 0 and 7")
         player_row = int(input("Choose your ships row: \n"))
         player_col = int(input("Choose your ships column: \n"))
     else:
         player_board[player_row][player_col] = "S"
-
+    """
+    for loop that gives the player 10 attempts at guessing
+    the computers ship coordinates
+    """
     for attempt in range(10):
         print("attempt", attempt + 1)
         player_guess_row = int(input("Guess your opponents row: \n"))
         player_guess_col = int(input("Guess your opponents column: \n"))
         print(f"Our target is ({player_guess_row},{player_guess_col}) FIRE!!!")
-
+        """
+        Below are the varibles which the comuters uses to guess the players
+        coordinates of thier ship
+        """
         comp_guess_row = round(randint(0, len(player_board)-1))
         comp_guess_col = round(randint(0, len(player_board)-1))
 
+        """
+        The if statement below checks the coordinates that were given
+        to see if the choice hits, misses, or if its been previously hit
+        It then goes on to check and remove 1 from either the players
+        or computers board if the ship was hit
+
+        """
         if (player_guess_row < int(0) or player_guess_row > int(7)) or  \
                 (player_guess_col < 0 or player_guess_col > 7):
             print("You\'re way off course solider!!!")
@@ -114,13 +174,23 @@ def game(comp_board, comp_col, comp_row, comp_board_app, player_board):
         else:
             player_board[comp_guess_row][comp_guess_col] = "X"
             print("Enemy: Swing and a miss...")
-
-        print("Oponants Board")
+        """
+        Here the varbiles are used to update both boards
+        for the player and computer
+        """
+        print("The Enemies Board")
         comp_board_app(comp_board)
         print(f"{players_name} board")
         player_board_app(player_board)
 
-        if attempt == 29:
+        """
+        Here the if statement checks the number of attempts
+        9 here represents 10, as te game starts at 0.
+        if attempts = 10, game is over.
+        if a computers ship is destyroyed print messgae.
+        if players ship is destroyed print message.
+        """
+        if attempt == 9:
             print("Damn the lack of torpedos... GAME OVER!")
             break
         else:
@@ -134,4 +204,7 @@ def game(comp_board, comp_col, comp_row, comp_board_app, player_board):
             break
 
 
+"""
+This is the game function being called to play the game
+"""
 game(comp_board, comp_col, comp_row, comp_board_app, player_board)
